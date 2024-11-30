80245ffbc34687bdff4e53353888c593bf86f9d7
Olivier van Helden <magic@miley.magiiic.com>
2024-11-30 21:55:01 +0100

    added main settings page

--- includes/class-osprojects-settings.php
+++ includes/class-osprojects-settings.php
@@ -9,6 +9,11 @@
 
 class OSProjectsSettings {
     /**
+     * Default URL prefix
+     */
+    const DEFAULT_PROJECT_URL_PREFIX = 'projects';
+
+    /**
      * Constructor
      */
     public function __construct() {
@@ -22,7 +27,39 @@ class OSProjectsSettings {
     public function init() {
         // Add settings page
         add_action( 'admin_menu', array( $this, 'add_settings_page' ) );
+        add_action( 'admin_init', array( $this, 'register_settings' ) );
 
+        // Set default project URL prefix if not set
+        $options = get_option( 'osprojects-settings' );
+        if ( ! isset( $options['project_url_prefix'] ) ) {
+            $options['project_url_prefix'] = self::DEFAULT_PROJECT_URL_PREFIX;
+            update_option( 'osprojects-settings', $options );
+        }
+    }
+
+    public function register_settings() {
+        register_setting( 'osprojects-settings-group', 'osprojects-settings' );
+
+        add_settings_section(
+            'osprojects-settings-section',
+            __( 'OSProjects Settings', 'osprojects' ),
+            null,
+            'osprojects-settings'
+        );
+
+        add_settings_field(
+            'project_url_prefix',
+            __( 'Project URL Prefix', 'osprojects' ),
+            array( $this, 'project_url_prefix_callback' ),
+            'osprojects-settings',
+            'osprojects-settings-section'
+        );
+    }
+
+    public function project_url_prefix_callback() {
+        $options = get_option( 'osprojects-settings' );
+        $project_url_prefix = isset( $options['project_url_prefix'] ) ? $options['project_url_prefix'] : self::DEFAULT_PROJECT_URL_PREFIX;
+        echo '<input type="text" name="osprojects-settings[project_url_prefix]" value="' . esc_attr( $project_url_prefix ) . '" />';
     }
 
     /**
@@ -48,3 +85,4 @@ class OSProjectsSettings {
         require_once OSPROJECTS_PATH . 'templates/settings.php';
     }
 }
+
--- templates/settings.php
+++ templates/settings.php
@@ -9,3 +9,9 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 echo "<h1>OSProjects Settings</h1>";
+
+echo '<form method="post" action="options.php">';
+settings_fields( 'osprojects-settings-group' );
+do_settings_sections( 'osprojects-settings' );
+submit_button();
+echo '</form>';
