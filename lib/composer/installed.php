0241ba78c0be72fdee80d258f5bcd7fc1f21aa5a
Olivier van Helden <magic@miley.magiiic.com>
2024-12-01 09:25:00 +0100

    added czproject/git-php lib

---
+++ lib/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'magicoli/osprojects',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '59c55e455a14d4cdef019306be070598fe2b666a',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'czproject/git-php' => array(
+            'pretty_version' => 'v4.3.0',
+            'version' => '4.3.0.0',
+            'reference' => 'f7fe85cc526c5f8e7ed809f7f9c68f2696940a8a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../czproject/git-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'magicoli/osprojects' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '59c55e455a14d4cdef019306be070598fe2b666a',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
