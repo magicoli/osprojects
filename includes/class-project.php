magicoli/osprojects

commit 5353ff395164ed9eddf96331c091ae1923ed069a

merge General and Repository metaboxes into Project Details metabox

includes/class-project.php
@@ -93,23 +93,39 @@ class OSProjectsProject {
             return;
         }
 
-        add_meta_box(
-            'general_meta_box',
-            __( 'General', 'osprojects' ),
-            array( $this, 'render_general_meta_box' ),
-            'project',
-            'normal',
-            'high'
-        );
+        // Remove existing "General" and "Repository" meta boxes
+        // add_meta_box(
+        //     'general_meta_box',
+        //     __( 'General', 'osprojects' ),
+        //     array( $this, 'render_general_meta_box' ),
+        //     'project',
+        //     'normal',
+        //     'high'
+        // );
 
+        // add_meta_box(
+        //     'repository_meta_box',
+        //     __( 'Repository', 'osprojects' ),
+        //     array( $this, 'render_repository_meta_box' ),
+        //     'project',
+        //     'normal',
+        //     'high'
+        // );
+
+        // Add new "Project Details" meta box
         add_meta_box(
-            'repository_meta_box',
-            __( 'Repository', 'osprojects' ),
-            array( $this, 'render_repository_meta_box' ),
+            'project_details_meta_box',
+            __( 'Project Details', 'osprojects' ),
+            array( $this, 'render_project_details_meta_box' ),
             'project',
             'normal',
             'high'
         );
+    }
+
+    public function render_project_details_meta_box( $post ) {
+        wp_nonce_field( 'save_project_details', 'project_details_nonce' );
+        require OSPROJECTS_PLUGIN_PATH . 'templates/project-fields-details.php';
     }
 
     /**
@@ -291,7 +307,6 @@ class OSProjectsProject {
      * AJAX handler to fetch Git data
      */
     public function ajax_fetch_git_data() {
-        error_log( __FUNCTION__ . ' called' );
         
         // Verify nonce with updated action name
         if ( ! isset( $_POST['nonce'] ) || ! wp_verify_nonce( $_POST['nonce'], 'osprojects_fetch_git_data' ) ) { // Updated nonce action
@@ -322,11 +337,11 @@ class OSProjectsProject {
 
         // Prepare the response data
         $data = array(
-            'license'                  => $license,
-            'version'                  => $version,
-            'release_date'             => $release_date,
-            'last_release_html'        => $last_release_html,
-            'last_commit_html'         => $last_commit_html,
+            'license'           => $license,
+            'version'           => $version,
+            'release_date'      => $release_date,
+            'last_release_html' => $last_release_html,
+            'last_commit_html'  => $last_commit_html,
         );
 
         wp_send_json_success( $data );

templates/project-fields-general.php
@@ -1,14 +1,0 @@
-<?php
-$project_website = get_post_meta( $post->ID, 'osp_project_website', true );
-?>
-
-<table class="form-table">
-    <tr>
-        <th scope="row">
-            <label for="osprojects_project_website"><?php _e( 'Project Website', 'osprojects' ); ?></label>
-        </th>
-        <td>
-            <input type="url" name="osprojects_project_website" id="osprojects_project_website" value="<?php echo esc_attr( $project_website ); ?>" class="regular-text" />
-        </td>
-    </tr>
-</table>

js/admin-scripts.js
@@ -62,7 +62,7 @@ jQuery(document).ready(function($) {
                         // Check if last_commit_html exists to validate the repository
                         if (response.data.last_commit_html) {
                             // Update the UI with the new data
-                            $('#osp_project_license').text(response.data.license || ''); // Allow empty license
+                            $('#osp_project_license').text(response.data.license || ''); // Ensure license is updated
                             $('#osp_project_release').html(response.data.last_release_html || ''); // Allow empty release
                             $('#osp_project_last_commit').html(response.data.last_commit_html);
                             
@@ -147,7 +147,7 @@ jQuery(document).ready(function($) {
                         // Check if last_commit_html exists to validate the repository
                         if (response.data.last_commit_html) {
                             // Update the UI with the new data
-                            $('#osp_project_license').text(response.data.license || ''); // Allow empty license
+                            $('#osp_project_license').text(response.data.license || ''); // Ensure license is updated
                             $('#osp_project_release').html(response.data.last_release_html || ''); // Allow empty release
                             $('#osp_project_last_commit').html(response.data.last_commit_html);
                             

templates/project-fields-details.php
@@ -1,12 +1,25 @@
+
 <?php
 $short_description = get_post_meta( $post->ID, 'osp_project_shortdesc', true );
 $project_repository = get_post_meta( $post->ID, 'osp_project_repository', true );
 $project_license = get_post_meta( $post->ID, 'osp_project_license', true );
 $project_release = get_post_meta( $post->ID, 'osp_project_last_release_html', true ); 
 $last_commit_html = get_post_meta( $post->ID, 'osp_project_last_commit_html', true );
+$project_website = get_post_meta( $post->ID, 'osp_project_website', true );
 ?>
 
 <table class="form-table">
+    <!-- General Fields -->
+    <tr>
+        <th scope="row">
+            <label for="osprojects_project_website"><?php _e( 'Project Website', 'osprojects' ); ?></label>
+        </th>
+        <td>
+            <input type="url" name="osprojects_project_website" id="osprojects_project_website" value="<?php echo esc_attr( $project_website ); ?>" class="regular-text" />
+        </td>
+    </tr>
+    
+    <!-- Repository Fields -->
     <tr>
         <th scope="row">
             <label for="osp_project_repository"><?php _e( 'Git Repository', 'osprojects' ); ?></label>

commit aa1c82f44a686878cc2fb688ff94c036bbcd1785

fix regression introduced in 5353ff3 (Project details updated on screen but not saved in custom fields)

includes/class-project.php
@@ -129,14 +129,14 @@ class OSProjectsProject {
             return;
         }
 
-        add_meta_box(
-            'general_meta_box',
-            __( 'General', 'osprojects' ),
-            array( $this, 'render_general_meta_box' ),
-            'project',
-            'normal',
-            'high'
-        );
+        // add_meta_box(
+        //     'general_meta_box',
+        //     __( 'General', 'osprojects' ),
+        //     array( $this, 'render_general_meta_box' ),
+        //     'project',
+        //     'normal',
+        //     'high'
+        // );
 
         add_meta_box(
             'repository_meta_box',
@@ -148,13 +148,13 @@ class OSProjectsProject {
         );
     }
 
-    /**
-     * Render the general meta box
-     */
-    public function render_general_meta_box( $post ) {
-        wp_nonce_field( 'save_general', 'general_nonce' );
-        require OSPROJECTS_PLUGIN_PATH . 'templates/project-fields-general.php';
-    }
+    // /**
+    //  * Render the general meta box
+    //  */
+    // public function render_general_meta_box( $post ) {
+    //     wp_nonce_field( 'save_general', 'general_nonce' );
+    //     require OSPROJECTS_PLUGIN_PATH . 'templates/project-fields-general.php';
+    // }
 
     /**
      * Render the repository meta box
@@ -218,9 +218,9 @@ class OSProjectsProject {
      */
     public function save_project_meta_boxes( $post_id ) {
         // Check if our nonce is set.
-        if ( ! isset( $_POST['general_nonce'] ) || ! wp_verify_nonce( $_POST['general_nonce'], 'save_general' ) ) {
-            return;
-        }
+        // if ( ! isset( $_POST['general_nonce'] ) || ! wp_verify_nonce( $_POST['general_nonce'], 'save_general' ) ) {
+        //     return;
+        // }
 
         if ( ! isset( $_POST['repository_nonce'] ) || ! wp_verify_nonce( $_POST['repository_nonce'], 'save_repository' ) ) {
             return;

templates/project-fields-general.php
@@ -1,14 +1,0 @@
-<?php
-$project_website = get_post_meta( $post->ID, 'osp_project_website', true );
-?>
-
-<table class="form-table">
-    <tr>
-        <th scope="row">
-            <label for="osprojects_project_website"><?php _e( 'Project Website', 'osprojects' ); ?></label>
-        </th>
-        <td>
-            <input type="url" name="osprojects_project_website" id="osprojects_project_website" value="<?php echo esc_attr( $project_website ); ?>" class="regular-text" />
-        </td>
-    </tr>
-</table>

templates/project-fields-repository.php
@@ -4,9 +4,18 @@ $project_repository = get_post_meta( $post->ID, 'osp_project_repository', true )
 $project_license = get_post_meta( $post->ID, 'osp_project_license', true );
 $project_release = get_post_meta( $post->ID, 'osp_project_last_release_html', true ); 
 $last_commit_html = get_post_meta( $post->ID, 'osp_project_last_commit_html', true );
+$osp_project_website = get_post_meta( $post->ID, 'osp_project_website', true );
 ?>
 
 <table class="form-table">
+    <tr>
+        <th scope="row">
+            <label for="osp_project_website"><?php _e( 'Project Website', 'osprojects' ); ?></label>
+        </th>
+        <td>
+            <input type="url" name="osp_project_website" id="osp_project_website" value="<?php echo esc_attr( $osp_project_website ); ?>" class="regular-text" />
+        </td>
+    </tr>
     <tr>
         <th scope="row">
             <label for="osp_project_repository"><?php _e( 'Git Repository', 'osprojects' ); ?></label>